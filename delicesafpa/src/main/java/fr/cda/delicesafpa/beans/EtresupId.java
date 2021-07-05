9115654f379bae100483e904f375af1739c62502
LucaToscana <78204916+LucaToscana@users.noreply.github.com>
2021-07-05 09:42:56 +0200

    ciao

---
+++ delicesafpa/src/main/java/fr/cda/delicesafpa/beans/EtresupId.java
@@ -0,0 +1,78 @@
+package fr.cda.delicesafpa.beans;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+
+
+
+//@Embeddable
+public class EtresupId implements Serializable {
+	
+	/*
+	private Article idarticle;
+	
+
+	private  Article idarticle_etreSup;
+
+	public EtresupId() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public EtresupId(Article idarticle, Article idarticle_etreSup) {
+		super();
+		this.idarticle = idarticle;
+		this.idarticle_etreSup = idarticle_etreSup;
+	}
+
+	public Article getIdarticle() {
+		return idarticle;
+	}
+
+	public void setIdarticle(Article idarticle) {
+		this.idarticle = idarticle;
+	}
+
+	public Article getIdarticle_etreSup() {
+		return idarticle_etreSup;
+	}
+
+	public void setIdarticle_etreSup(Article idarticle_etreSup) {
+		this.idarticle_etreSup = idarticle_etreSup;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idarticle == null) ? 0 : idarticle.hashCode());
+		result = prime * result + ((idarticle_etreSup == null) ? 0 : idarticle_etreSup.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EtresupId other = (EtresupId) obj;
+		if (idarticle == null) {
+			if (other.idarticle != null)
+				return false;
+		} else if (!idarticle.equals(other.idarticle))
+			return false;
+		if (idarticle_etreSup == null) {
+			if (other.idarticle_etreSup != null)
+				return false;
+		} else if (!idarticle_etreSup.equals(other.idarticle_etreSup))
+			return false;
+		return true;
+	}
+	
+	*/
+
+}
